1cc16382842447bb93c7a670f5a357cc34158909
MuhammadNur <delusionboys974@gmail.com>
2024-09-12 13:17:03 +0700

    structure logic register

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+
+	"github.com/codingsluv/crowdfounding/handler"
+	"github.com/codingsluv/crowdfounding/user"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	// import "gorm.io/driver/mysql"
+	// refer: https://gorm.io/docs/connecting_to_the_database.html#MySQL
+	dsn := "root:codingsluv@tcp(127.0.0.1:3306)/crowdfounding?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	userRepository := user.NewRepository(db)
+	userService := user.NewSevice(userRepository)
+
+	userHandler := handler.NewUserHandler(userService)
+
+	router := gin.Default()
+	api := router.Group("/api/v1")
+	api.POST("/users", userHandler.RegisterUser)
+	router.Run(":8080")
+
+}

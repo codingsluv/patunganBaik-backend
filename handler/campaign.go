ffcb942999bee9f0c7ef1ba6a8bed3df35e3cdf1
MuhammadNur <delusionboys974@gmail.com>
2024-09-26 21:45:39 +0700

    [wip] findById campaign

--- handler/campaign.go
+++ handler/campaign.go
@@ -37,3 +37,24 @@ func (h *campaignHandler) GetCampaigns(c *gin.Context) {
 	response := helper.ApiResponse("List of campaigns", http.StatusOK, "success", campaign.FormatCampaigns(campaigns))
 	c.JSON(http.StatusOK, response)
 }
+
+func (h *campaignHandler) GetCampaign(c *gin.Context) {
+	var input campaign.GetCampaignDetailInput
+
+	err := c.ShouldBindJSON(&campaign.Campaign{})
+	if err != nil {
+		response := helper.ApiResponse("Error to get detail campaign", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	campignDetail, err := h.service.GetCampaignDetailInputs(input)
+	if err != nil {
+		response := helper.ApiResponse("Failed to get detail campaign", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	response := helper.ApiResponse("Detail campaign", http.StatusOK, "success", campaign.FormatCampaign(campignDetail))
+	c.JSON(http.StatusOK, response)
+
+}
